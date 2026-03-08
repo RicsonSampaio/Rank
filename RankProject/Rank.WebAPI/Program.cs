360d95b49ac95ed3e4ee71f19beae82fc6a2e8c7
Ricson <ricson@construcode.com>
2026-03-07 23:05:48 -0300

    deploy_07_03_2026

--- RankProject/Rank.WebAPI/Program.cs
+++ RankProject/Rank.WebAPI/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World! deploy_07_03_2026 ");
 
 app.Run();
