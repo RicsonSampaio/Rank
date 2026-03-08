6fc9586f6567f532cb148e6d6898f0d41c81cf8a
Ricson <ricson@construcode.com>
2026-03-07 23:13:08 -0300

    first commit pack_1

--- RankProject/Rank.WebAPI/Program.cs
+++ RankProject/Rank.WebAPI/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! deploy_07_03_2026 ");
+app.MapGet("/", () => "Hello World! pack_1 ");
 
 app.Run();
